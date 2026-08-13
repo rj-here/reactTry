fee3e9778c28f4b7765f60bfa49b75234f28bb56
rj-here <r.rishi.rj@gmail.com>
2026-08-13 17:47:25 -0400

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,31 +13,52 @@ function App() {
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-        <div>
           <h1>My Summer Reads</h1>
           <p>
             I'm trying to create a basic React project. I thought of just sharing my summer reads as a starting point.
           </p>
+        <div id="books">
+        <ul>
+          <h2>Deep Work: Rules for Focused Success in a Distracted World</h2>
+          <p>Cal Newport</p>            
+        </ul>
+        <ul>
+          <h2>Modern Friendships: How to Nurture Our Most Valued Connections</h2>
+          <p>Anna Goldferb</p>
+        </ul>
+        <ul>
+          <h2>Platonic: How the Science of Attachment Can Help You Make—And Keep—Friends</h2>
+          <p>Marisa G. Franco</p>
+        </ul>
+        <ul>
+          <h2>Stranger Things: The Dustin Experiment</h2>
+          <p>J.L.D'Amato</p>
+        </ul>
+        <ul>
+          <h2>Feel Good Productivity: How to Do More of What Matters to You</h2>
+          <p>Ali Abdaal</p>
+        </ul>
         </div>
-        
       </section>
-
-      <div className="ticks"></div>
-
       <section id="next-steps">
         <div id="social">
+          <h1>Connect with me</h1>
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
-  
           <ul>
-            
+            <li>
+              <a href="">LinkedIn</a>
+            </li>
+            <li>
+              <a href="">GitHub</a>
+            </li>
+            <li>
+              <a href="">Portfolio</a>
+            </li>
           </ul>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
